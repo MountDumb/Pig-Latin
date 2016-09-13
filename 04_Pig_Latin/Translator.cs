217e5cdb408e8726203bbcb6c5910e5e562dc254
UrbanSkurk <UrbanSkurk@HELL>
2016-09-13 11:11:19 +0200

    0.1

--- 04_Pig_Latin/Translator.cs
+++ 04_Pig_Latin/Translator.cs
@@ -1,10 +1,57 @@
-﻿namespace _04_Pig_Latin
+﻿//using System;
+//using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_Pig_Latin
 {
     internal class Translator
     {
         public Translator()
         {
             
+        }
+
+        public string Translate(string input)
+        {
+            string[] pigout = input.Split();
+
+            if (pigout.Length == 1)
+            {
+                return Piggify(pigout[0]);
+            }
+            else
+            {
+                for (int i = 0; i < pigout.Length; i++)
+                {
+                    pigout[i] = Piggify(pigout[i]);
+                }
+            }
+
+            return string.Join(" ", pigout);
+           
+
+        }
+
+        public string Piggify(string input)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o' };
+            string output = input;
+            bool running = true;
+            
+            while (running)
+            { 
+            if (!vowels.Contains(char.Parse(output.Substring(0, 1))))
+            {
+                output = output.Substring(1) + output.Substring(0, 1);
+            }
+            else
+                {
+                    output += "ay";
+                    running = false;
+                }
+            }
+
+            return output;
         }
     }
 }
